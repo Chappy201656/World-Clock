fc73a78b49edd94838423f0cac80ff2da6caa32a
Chappy201656 <133194626+Chappy201656@users.noreply.github.com>
2024-09-23 16:43:53 -0400

    Added current location

--- javascript/java.js
+++ javascript/java.js
@@ -42,6 +42,9 @@ setInterval(updateTime, 1000);
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+        cityTimeZone = moment.tz.guess();
+    }
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
@@ -51,7 +54,7 @@ function updateCity(event) {
           <h2>${cityName}</h2>
           <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
-        <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div>
+        <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
       </div>
     `
 }
